bc381fb06836293f5f8ab925d7390ee4002463a4
Mathieu Fauvel <mfauvel@fauvel-laptop>
2020-01-07 17:22:58 +0100

    Update introduction

--- Courses/Introduction/auto/intro.el
+++ Courses/Introduction/auto/intro.el
@@ -6,12 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("babel" "english") ("doclicense" "type={CC}" "modifier={by-sa}" "version={4.0}" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -56,13 +57,7 @@
     "bx"
     "bbeta"
     "balpha"
-    "bxi"
-    "var"
-    "cov"
-    "trace"
-    "rank"
-    "logit"
-    "sign")
+    "bxi")
    (LaTeX-add-bibitems
     "A"
     "B"
